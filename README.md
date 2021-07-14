0da318530fdf246d571d59785ae12d69a4092414
danhhuynh25029 <danhhuynh25029@gmail.com>
2021-07-14 20:27:52 +0700

    add table

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # terminal-app
+
+![api-covid19][0]
+![api-weather][1]
+
+[0]:https://api.covid19api.com/summary
--- main.py
+++ main.py
@@ -1,36 +1,49 @@
 import argparse
 import os
 import requests
+from prettytable import PrettyTable
+def Print():
+    pass
 def getCovid():
     data = requests.get("https://api.covid19api.com/summary")
     json = data.json()
     l = json["Countries"]
+    tb = PrettyTable(["Country","TotalConfirmed","TotalDeaths"])
     for i in l:
         if i["CountryCode"] == "VN":
-            print("Country\tTotalConfirmed\tTotalDeaths")
-            print("{}\t{}\t{}".format(i["Country"],i["TotalConfirmed"],i["TotalDeaths"]))
+            #print("Country\tTotalConfirmed\tTotalDeaths")
+            tb.add_row([i["Country"],i["TotalConfirmed"],i["TotalDeaths"]])
+            print(tb)
+            #print("{}\t{}\t{}".format(i["Country"],i["TotalConfirmed"],i["TotalDeaths"]))
             break
 def getWeather():
     myKey = "e90072222e60645e2f9ed2949daec557"
     data = requests.get("https://api.openweathermap.org/data/2.5/weather?q=Tra%20Vinh&appid="+myKey)
     json = data.json()
     weather = json["weather"]
+    #print(weather[0]["description"])
     temp = json["main"]
-    print("City\tdescription\ttemp")
-    print("{}\t{}\t{}".format(json["name"],weather[0]["description"],str(temp["temp"]/10)+u"\N{DEGREE SIGN}"+"C"))
+    tb = PrettyTable(["city","description","temp"])
+    tb.add_row([json["name"],weather[0]["description"],str(round(temp["temp"]/10,2))+u"\N{DEGREE SIGN}"+"C"])
+    print(tb)
+    #print("City\tdescription\ttemp")
+    #print("{}\t{}\t{}".format(json["name"],weather[0]["description"],str(round(temp["temp"]/10,2))+u"\N{DEGREE SIGN}"+"C"))
 def getMemory():
-    cpu = os.popen("free -t -m").readlines()[-1]
-    a = list(map(int,cpu.split()[1:]))
+    memory = os.popen("free -t -m").readlines()
+    for i in memory:
+        print(i[:-1])
+    memoryUsage = memory[-1]
+    a = list(map(int,memoryUsage.split()[1:]))
     print("Memory usage : ",round(a[1]/a[0],2)*100,"%")
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("-u","--usage",action="store_true",help="Memory usage")
+    ap.add_argument("-m","--memory",action="store_true",help="Memory usage")
     ap.add_argument("-t","--time",action="store_true",help="Datetime")
     ap.add_argument("-w","--weather",action="store_true",help="Weather")
     ap.add_argument("-c","--covid",action="store_true",help="Information covid")
     args = vars(ap.parse_args())
     #print(args)
-    if args["usage"] == True:
+    if args["memory"] == True:
         #print("CPU usage")
         getMemory()
     elif args["time"] == True:
