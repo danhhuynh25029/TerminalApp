1947835f526d30214510012a8082559d667f096b
danhhuynh25029 <danhhuynh25029@gmail.com>
2021-07-14 10:25:05 +0700

    add command datetim

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import argparse
-ap = argparse.ArgumentParser()
-ap.add_argument("-u",action="store_const",help="CPU usage",const="y")
-ap.add_argument("-t",action="store_const",help="Datetime",const="y")
-args = vars(ap.parse_args())
-if args["u"] == "y":
-    print("CPU usage")
-elif args["t"] == "y":
-    print("Datetime")
+import os
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-u",action="store_const",help="CPU usage",const="y")
+    ap.add_argument("-t",action="store_const",help="Datetime",const="y")
+    args = vars(ap.parse_args())
+    if args["u"] == "y":
+        print("CPU usage")
+    elif args["t"] == "y":
+        s = os.popen("date").readline()[:-1]
+        print("Datetime now : ",s)
+if __name__ == "__main__":
+    main()
