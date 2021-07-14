be0009ffe78354674ef57d78df36b152c797e094
danhhuynh25029 <danhhuynh25029@gmail.com>
2021-07-14 21:08:31 +0700

    add code

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ import requests
 from prettytable import PrettyTable
 def Print():
     pass
-def getCovid():
+def getCovid(country):
     data = requests.get("https://api.covid19api.com/summary")
     json = data.json()
     l = json["Countries"]
     tb = PrettyTable(["Country","TotalConfirmed","TotalDeaths"])
+    choose = 0
+    if country  == "all":
+        choose = 1
     for i in l:
-        if i["CountryCode"] == "VN":
-            #print("Country\tTotalConfirmed\tTotalDeaths")
+        if choose == 0:
+            if i["CountryCode"] == country:
+                #print("Country\tTotalConfirmed\tTotalDeaths")
+                tb.add_row([i["Country"],i["TotalConfirmed"],i["TotalDeaths"]])
+                print(tb)
+                #print("{}\t{}\t{}".format(i["Country"],i["TotalConfirmed"],i["TotalDeaths"]))
+                break
+        else: 
             tb.add_row([i["Country"],i["TotalConfirmed"],i["TotalDeaths"]])
             print(tb)
-            #print("{}\t{}\t{}".format(i["Country"],i["TotalConfirmed"],i["TotalDeaths"]))
-            break
 def getWeather():
     myKey = "e90072222e60645e2f9ed2949daec557"
     data = requests.get("https://api.openweathermap.org/data/2.5/weather?q=Tra%20Vinh&appid="+myKey)
@@ -37,6 +44,7 @@ def getMemory():
     print("Memory usage : ",round(a[1]/a[0],2)*100,"%")
 def main():
     ap = argparse.ArgumentParser(description="Terminal app")
+    ap.add_argument("x",help="choose country information covid19 (name/all)")
     ap.add_argument("-m","--memory",action="store_true",help="Memory usage")
     ap.add_argument("-t","--time",action="store_true",help="Datetime")
     ap.add_argument("-w","--weather",action="store_true",help="Weather")
@@ -52,6 +60,6 @@ def main():
     elif args["weather"] == True:
         getWeather()
     elif args["covid"] == True:
-        getCovid()
+        getCovid(args["x"])
 if __name__ == "__main__":
     main()
