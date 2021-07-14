53918a88fb8cbbf0b128ed87a9bf336dcd065575
danhhuynh25029 <danhhuynh25029@gmail.com>
2021-07-14 13:49:53 +0700

    change code

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import argparse
 import os
 def getWeather():
     pass
-def getCPU():
+def getMemory():
     cpu = os.popen("free -t -m").readlines()[-1]
-    print("CPU usage : ",cpu[:-1])
+    a = list(map(int,cpu.split()[1:]))
+    print("Memory usage : ",round(a[1]/a[0],2)*100,"%")
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("-u","--usage",action="store_true",help="CPU usage")
+    ap.add_argument("-u","--usage",action="store_true",help="Memory usage")
     ap.add_argument("-t","--time",action="store_true",help="Datetime")
     ap.add_argument("-w","--weather",action="store_true",help="Weather")
     args = vars(ap.parse_args())
     #print(args)
     if args["usage"] == True:
         #print("CPU usage")
-        getCPU()
+        getMemory()
     elif args["time"] == True:
         s = os.popen("date").readline()[:-1]
         print("Datetime now : ",s)
