fef83dc1c46ff8898eb69157785aebe00224a8fd
danhhuynh25029 <danhhuynh25029@gmail.com>
2021-07-14 13:35:16 +0700

    add code in file main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import argparse
 import os
+def getWeather():
+    pass
 def getCPU():
     cpu = os.popen("free -t -m").readlines()[-1]
     print("CPU usage : ",cpu[:-1])
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("-u",action="store_const",help="CPU usage",const="y")
-    ap.add_argument("-t",action="store_const",help="Datetime",const="y")
+    ap.add_argument("-u","--usage",action="store_true",help="CPU usage")
+    ap.add_argument("-t","--time",action="store_true",help="Datetime")
+    ap.add_argument("-w","--weather",action="store_true",help="Weather")
     args = vars(ap.parse_args())
-    if args["u"] == "y":
+    #print(args)
+    if args["usage"] == True:
         #print("CPU usage")
         getCPU()
-    elif args["t"] == "y":
+    elif args["time"] == True:
         s = os.popen("date").readline()[:-1]
         print("Datetime now : ",s)
 if __name__ == "__main__":
