8788d05571f67f0730f3f75b44badd4b807a6094
danhhuynh25029 <danhhuynh25029@gmail.com>
2021-07-14 10:39:45 +0700

    add function cpu usage

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import argparse
 import os
+def getCPU():
+    cpu = os.popen("free -t -m").readlines()[-1]
+    print("CPU usage : ",cpu[:-1])
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-u",action="store_const",help="CPU usage",const="y")
     ap.add_argument("-t",action="store_const",help="Datetime",const="y")
     args = vars(ap.parse_args())
     if args["u"] == "y":
-        print("CPU usage")
+        #print("CPU usage")
+        getCPU()
     elif args["t"] == "y":
         s = os.popen("date").readline()[:-1]
         print("Datetime now : ",s)
