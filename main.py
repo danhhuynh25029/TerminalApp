8c36ac04b7163d431d449e4bfabf1a4f11ffbd93
danhhuynh25029 <danhhuynh25029@gmail.com>
2021-07-14 14:43:11 +0700

    add function covid

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import argparse
 import os
+import requests
+def getCovid():
+    data = requests.get("https://api.covid19api.com/summary")
+    json = data.json()
+    l = json["Countries"]
+    for i in l:
+        if i["CountryCode"] == "VN":
+            print("Country\tTotalConfirmed\tTotalDeaths")
+            print("{}\t{}\t{}".format(i["Country"],i["TotalConfirmed"],i["TotalDeaths"]))
+            break
 def getWeather():
     pass
 def getMemory():
@@ -11,6 +21,7 @@ def main():
     ap.add_argument("-u","--usage",action="store_true",help="Memory usage")
     ap.add_argument("-t","--time",action="store_true",help="Datetime")
     ap.add_argument("-w","--weather",action="store_true",help="Weather")
+    ap.add_argument("-c","--covid",action="store_true",help="Information covid")
     args = vars(ap.parse_args())
     #print(args)
     if args["usage"] == True:
@@ -19,5 +30,9 @@ def main():
     elif args["time"] == True:
         s = os.popen("date").readline()[:-1]
         print("Datetime now : ",s)
+    elif args["weather"] == True:
+        getWeather()
+    elif args["covid"] == True:
+        getCovid()
 if __name__ == "__main__":
     main()
